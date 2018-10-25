7da8e4c006293e22097bac520d0d1b661edc8443
jruppent <jruppent@iwu.edu>
2018-10-24 21:51:37 -0500

    Created app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    Budgeting Application
+    ~~~~~~~~~~~~~~~~~~~~~~
+
+    An application for users to enter incomes and
+    expenses so they can budget their money efficiently
+
+    Authors: Joe Ruppenthal, Jonathan Nocek, Johnny Whitfield,
+             and Alex O'Neill
+"""
+
